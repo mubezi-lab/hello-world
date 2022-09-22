c646770d36dbe2c0d2c4b72ef99a9be7809a80f1
mubezi-lab <mubezigraphic@gmail.com>
2022-09-22 18:15:01 +0300

    addind word javascript file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const initGame = () => {
     let randomObj = words[Math.floor(Math.random() * words.length)];
-    let wordArray = randomObj.words.split(" ");
+    let wordArray = randomObj.word.split(" ");
     console.log(wordArray);
 }
 
---
+++ words.js
@@ -0,0 +1,26 @@
+const words = [
+    {
+        word: "kokusima",
+        hint: "kabinti kazuri ka kwangu"
+    },
+    {
+        word: "emiliana",
+        hint: "jina la bibi koku"
+    },
+    {
+        word: "doreen",
+        hint: "mama wa wanangu"
+    },
+    {
+        word: "bubelwa",
+        hint: "katoto kangu ka kiume katundu"
+    },
+    {
+        word: "asia",
+        hint: "dada wa wanangu"
+    },
+    {
+        word: "joel",
+        hint: "kaka mkubwa"
+    }
+]
